ed6581fe2e92b96de7de78ace1ce2366ca8f3110
st4rkeey <st4rk3y@yandex.ru>
2023-12-16 12:43:37 +0300

    Parser is done

---
+++ README.md
@@ -0,0 +1,2 @@
+This program is parses data(quotes, authors and tags) from the website: 'https://quotes.toscrape.com/' and collects it to .csv file
+
--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import logging
 
+logging.basicConfig(level=logging.INFO)
+
+def parser(url: str) -> None:
+    result = requests.get(url=url)
+    soup = BeautifulSoup(result.text, 'lxml')
+    quotes = soup.find_all('span', class_='text')
+    authors = soup.find_all('small', class_='author')
+    tags = soup.find_all('div', class_='tags')
+
+    for i in range(0, len(quotes)):
+        writing_csv(quotes[i].text, authors[i].text, tags[i].text)
+
+def writing_csv(*args: str) -> None:
+    with open('file.csv', 'a') as f:
+        for i in args:
+            f.write(f'{i}\n')
+
+
+if __name__ == '__main__':
+    parser(url='https://quotes.toscrape.com/')
